d00e9abfb70d00f09c3f47ddea6c132ee9b08a78
sandesh1337 <sandeshkunwar.1337@gmail.com>
2024-02-21 14:59:42 +0000

    added parse.js

--- src/sql/insert.sql
+++ src/sql/insert.sql
@@ -1,1 +1,11 @@
- INSERT user 
+-- I want to insert all the games into game table users into user table and team into teams table
+
+-- Inserting into game table
+INSERT INTO game (game_id, game_name, game_type, game_date, game_time, game_location, game_description, game_status)
+
+-- Inserting into user table
+INSERT INTO users (user_id, user_name, user_email, user_password, user_role, user_status)
+
+-- Inserting into teams table
+INSERT INTO teams (team_id, team_name, team_description, team_status)
+```
